89aef847ea9b2c72c8e5dd16bbf1a31a7c6819f6
Ingy döt Net <ingy@ingy.net>
2024-07-28 07:44:09 -0700

    Some hygenic reformatting

--- bin/run.sh
+++ bin/run.sh
@@ -9,15 +9,18 @@
 # $3: path to output directory
 
 # Output:
-# Writes the test results to a results.json file in the passed-in output directory.
-# The test results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
+# Writes the test results to a results.json file in the passed-in output
+# directory.
+# The test results are formatted according to the specifications at
+# https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
 
 # Example:
 # ./bin/run.sh two-fer path/to/solution/folder/ path/to/output/directory/
 
-# If any required arguments is missing, print the usage and exit
+# If any required arguments is missing, print the usage and exit:
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "usage: ./bin/run.sh exercise-slug path/to/solution/folder/ path/to/output/directory/"
+    echo "usage: ./bin/run.sh exercise-slug " \
+         "path/to/solution/folder/ path/to/output/directory/"
     exit 1
 fi
 
@@ -26,37 +29,42 @@ solution_dir=$(realpath "${2%/}")
 output_dir=$(realpath "${3%/}")
 results_file="${output_dir}/results.json"
 
-# Create the output directory if it doesn't exist
+# Create the output directory if it doesn't exist:
 mkdir -p "${output_dir}"
 
 echo "${slug}: testing..."
 
 # Run the tests for the provided implementation file and redirect stdout and
-# stderr to capture it
+# stderr to capture it:
 test_output=$(prove ${solution_dir}/${slug}-test.ys 2>&1)
 rc=$?
 
-# Write the results.json file based on the exit code of the command that was 
-# just executed that tested the implementation file
+# Write the results.json file based on the exit code of the command that was
+# just executed that tested the implementation file:
 sanitized_test_output=$(printf "${test_output}" | sed '/wallclock/d')
 if [ $rc -eq 0 ]; then
     jq -n '{version: 1, status: "pass"}' > ${results_file}
 else
     if (echo "$sanitized_test_output" | grep -q '^  Failed tests: '); then
-        jq -n --arg output "${sanitized_test_output}" '{version: 1, status: "fail", message: $output}' > ${results_file}
+        jq -n --arg output "${sanitized_test_output}" \
+           '{version: 1, status: "fail", message: $output}' \
+           > ${results_file}
         # OPTIONAL: Sanitize the output
-        # In some cases, the test output might be overly verbose, in which case stripping
-        # the unneeded information can be very helpful to the student
+        # In some cases, the test output might be overly verbose, in which case
+        # stripping the unneeded information can be very helpful to the student
         # sanitized_test_output=$(printf "${test_output}" | sed -n '/Test results:/,$p')
 
-        # OPTIONAL: Manually add colors to the output to help scanning the output for errors
-        # If the test output does not contain colors to help identify failing (or passing)
-        # tests, it can be helpful to manually add colors to the output
-        # colorized_test_output=$(echo "${test_output}" \
-        #      | GREP_COLOR='01;31' grep --color=always -E -e '^(ERROR:.*|.*failed)$|$' \
-        #      | GREP_COLOR='01;32' grep --color=always -E -e '^.*passed$|$')
+        # OPTIONAL: Manually add colors to the output to help scanning the
+        # output for errors If the test output does not contain colors to help
+        # identify failing (or passing) tests, it can be helpful to manually
+        # add colors to the output.
+        # colorized_test_output=$(echo "${test_output}" |
+        #   GREP_COLOR='01;31' grep --color=always -E \
+        #     -e '^(ERROR:.*|.*failed)$|$' |
+        #   GREP_COLOR='01;32' grep --color=always -E -e '^.*passed$|$')
     else
-        jq -n --arg output "${sanitized_test_output}" '{version: 1, status: "error", message: $output}' > ${results_file}
+        jq -n --arg output "${sanitized_test_output}" \
+          '{version: 1, status: "error", message: $output}' > ${results_file}
     fi
 fi
 
